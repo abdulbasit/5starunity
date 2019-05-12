764569b05d7a1bd90f0688a96f3d15f837f183a3
Abdul <abdulbasit056@gmail.com>
2019-05-12 15:02:35 +0500

    lottery of things page working

--- resources/views/pages/lottery_things.blade.php
+++ resources/views/pages/lottery_things.blade.php
@@ -1,13 +1,23 @@
 @extends('layouts.app')
 @section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div id="historyTable">
+            <img src="{{URL::to('/') }}/images/1554660749_5starunity.jpg"}} style="width:100%" />
+        </div>
+    </div>
+</div>
 <div class="container">
     <div class="row profile">
-         <div class="col-md-9 prof">
+         <div class="col-md-9">
             <div class="checkout-panel">
-                <div id="historyTable">
-                        <img src="URL::to('/') }}/uploads/slider/1554660749_5starunity.jpg" />
-sd
-                </div>
+                <h4>
+                    Mit SPENDEN in die Zukunft tolle Lieblingsprodukte gewinnen
+                </h4>
+                <p>Ihre Spenden helfen zukunftsorientierten Unternehmen, Start-Ups, Entwicklern und Gründern dabei, diverse Konzepte / Produkte zu gestalten / realisieren.</p>
+                <p>Wir möchten Sie auf unseren Vorstellungsseiten der ERFINDER / GRÜNDER transparent über bereits erfolgte Unterstützungen informieren.</p>
+                <p>Neben der emotionalen Befriedigung geleisteter Hilfen ist ein weiterer Vorteil Ihrer Spende, dass Sie mit 5starUnity-Talern belohnt werden und nun die Möglichkeit besitzen, nachfolgende Sachpreise gewinnen zu können. </p>
+                <p>Lieblingsprodukte suchen, 5starUnity-Taler in Lose tauschen und mit ein wenig Fortuna, bei der anwaltlich beglaubigten Ziehung, tolle Gewinne erlangen.</p>
          </div>
     </div>
 </div>
