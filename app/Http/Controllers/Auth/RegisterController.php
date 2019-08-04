1e84ee122e8321bf94c86c8bd8bf8ba8c2c578c5
Abdul <abdulbasit056@gmail.com>
2019-08-04 01:49:21 -0700

    ddf

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -197,7 +197,7 @@ class RegisterController extends Controller
             //Move Uploaded File
            $id_proofImg = time().'_5star_id_proof.'.$identity_proofImage->getClientOriginalExtension();
            $identity_proofImage->move($idProofImg, $id_proofImg);
-
+           
             $product_images = UserDocument::create([
                 "user_id" => $user_id->id,
                 "res_proof"=>$id_proofImg,
