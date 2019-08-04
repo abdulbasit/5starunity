6bdf9500aa5534d469d9daee8ce41f3f15ce3043
Abdul <abdulbasit056@gmail.com>
2019-08-04 01:25:47 -0700

    fixes

--- resources/lang/de/messages.php
+++ resources/lang/de/messages.php
@@ -2,7 +2,7 @@
 return [
     'register_success_info' => 'Bitte überprüfen Sie Ihren Post-Eingang und verifizieren Sie die E-Mail-Adresse.',
     'delte_account'=>"Wollen Sie wirklich Ihre Konto löschen?",
-    'subscription_thanks'=>'Vielen Dank für Ihre Interesse',
+    'subscription_thanks'=>'Vielen Dank für Ihre Interesse ',
     'already_exists'=>'Bereits angemeldet'
 ];
 ?>
