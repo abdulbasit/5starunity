c2b4598369a182f98a9b7d341b4b3afcb0e5b9bf
Abdul <abdulbasit056@gmail.com>
2019-08-20 02:27:02 -0700

    dddd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -205,7 +205,7 @@
                     <div class="row">
                         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 BottomText grey_75 text-center">
                             <a style="text-decoration: none;cursor: default !important;">&copy; <?php echo date('Y') ?> 5starUnity e.V.</a>
-                            <a href="/age/terms">{{ __('menu.terms')}}</a>
+                            <a href="/page/terms">{{ __('menu.terms')}}</a>
                             <a href="/page/data-security">{{ __('menu.privacy')}}</a>
                             <a href="/impresum">{{ __('menu.legal_notice')}}</a>
                         </div>
