60a0765610c8b84918c14a37b5650ee24a3804c6
abdulbasit056 <abdulbaasit056@gmail.com>
2019-10-08 01:14:42 +0500

    ddd

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -86,8 +86,9 @@ class UserController extends Controller
             $dir_path = public_path()."/uploads/users/documents_proofs/id_proof";
             $zip_path = "/".$documents->id_front."/";
             // $this->addToZip($zip,$dir_path,$zip_path);
-            if (file_exists($dir_path.$zip_path) && is_file($dir_path.$zip_path))
+            // if (file_exists($dir_path.$zip_path) && is_file($dir_path.$zip_path))
             $zip->addFile($dir_path, $zip_path);
+            dd($dir_path, $zip_path);
             $zip->close();
             $name = $name.'.zip';
             header('Content-Type: application/zip');
