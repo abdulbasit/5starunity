5b1b9089c2d1d86608b894ecf4fa6b0cc441e5b0
abdulbasit056 <abdulbaasit056@gmail.com>
2019-10-08 00:37:45 +0500

    ddd

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -89,8 +89,9 @@ class UserController extends Controller
                 $zip = new ZipArchive;
                 if ($zip->open($public_dir . '/' . $zipFileName, ZipArchive::CREATE) === TRUE) {
                     // Add File in ZipArchive
+                    dd(public_path('uploads/users/documents_proofs/id_proof'),$documents->id_front);
                     $zip->addFile(public_path('uploads/users/documents_proofs/id_proof'),$documents->id_front);
-                    $zip->addFile(public_path('uploads/users/documents_proofs/id_proof'),$documents->id_back);
+                    // $zip->addFile(public_path('uploads/users/documents_proofs/id_proof'),$documents->id_back);
                     // Close ZipArchive   
                     $zip->close();
                 }
