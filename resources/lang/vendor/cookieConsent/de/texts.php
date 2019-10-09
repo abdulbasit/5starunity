19ee7a076b479b2f75000c2a52ea33b5ac1f3cf1
abdulbasit056 <abdulbaasit056@gmail.com>
2019-10-09 05:50:59 +0500

    cookies test chagned

---
+++ resources/lang/vendor/cookieConsent/de/texts.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'message' => 'Diese Webseite verwendet Cookies, von denen einige zur besseren Bedienbarkeit notwendig sind, andere dienen statistischen Zwecken und damit der Verbesserung unserer Inhalte. Benutzen Sie die EINSTELLUNGEN, um Cookies zu deaktivieren. Details finden Sie und der <a href="/page/data-security">DATENSCHUTZERKLÄRUNG</a>.',
+    'agree' => 'Akzeptieren',
+];
