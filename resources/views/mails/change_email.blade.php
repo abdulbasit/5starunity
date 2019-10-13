60909389c91fbc16d09203e6aed0dee3b6260e6d
abdulbasit056 <abdulbaasit056@gmail.com>
2019-10-13 23:35:47 +0500

    changes

--- resources/views/mails/change_email.blade.php
+++ resources/views/mails/change_email.blade.php
@@ -96,8 +96,6 @@
                                 </tr>
                                 <tr>
                                   <td align="center">
-                                    <p style="color:#a2a2a2; font-size:12px; line-height:17px; font-style:italic;">If you did not chagne email address for this account you can ignore this email and the account
-                                      will be deleted.</p>
                                   </td>
                                 </tr>
                               </tbody>
