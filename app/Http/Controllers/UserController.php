93eaa507c37f974fd173a2f78fa2fed21f079040
abdulbasit056 <abdulbaasit056@gmail.com>
2019-10-22 02:05:58 +0500

    dfdfdf

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -345,6 +345,7 @@ class UserController extends Controller
     }
     public function redirectToUrl($id)
     {
+        dd($id);a
         // $userId = Auth::guard('client')->user()->id;
         // $cuppon = DiscountCuppon::find($id);
         // $cupponCounter = CupponCount::where('cuppon_id',$id)->count();
