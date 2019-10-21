56ae4da8e2f00dc2cd41cd73493faf8ff12d823d
abdulbasit056 <abdulbaasit056@gmail.com>
2019-10-22 02:03:24 +0500

    dfdfd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -349,12 +349,12 @@ class UserController extends Controller
         $cuppon = DiscountCuppon::find($id);
         $cupponCounter = CupponCount::where('cuppon_id',$id)->count();
         // dd($id,$userId,$this->get_client_ip());
-        $cupponCounterCreate = CupponCount::create([
-            "cuppon_id"=>1,
-            "counter"=>"1",
-            "user_id"=>1,
-            "ip_address"=>"sadsa"
-        ]);
+        // $cupponCounterCreate = CupponCount::create([
+        //     "cuppon_id"=>1,
+        //     "counter"=>"1",
+        //     "user_id"=>1,
+        //     "ip_address"=>"sadsa"
+        // ]);
         return view('usr_profile.redirect',compact('cuppon'));
     }
     function get_client_ip() 
