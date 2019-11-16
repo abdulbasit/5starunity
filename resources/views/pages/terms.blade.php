22ff550a7883e1662199f8e03b377420a20a29df
abdulbasit056 <abdulbaasit056@gmail.com>
2019-11-16 14:43:15 +0500

    changes

--- resources/views/pages/terms.blade.php
+++ resources/views/pages/terms.blade.php
@@ -116,7 +116,7 @@
                             </div>
                         </div>
                     </form>
-                    <br />
+                    <br /><br /><br />
                     <div class="container" style=" border:solid 4px #ccc">
                         @foreach($promotionsResult as $key =>$promotions)
                             <div class="row" style="border-bottom:solid 1px #ccc; margin-bottom: 10px ">
